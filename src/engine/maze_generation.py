383b105f45e9b8c4bc42d56a6f1cf75aacca2351
Andrey Semenov <andreicemenov@gmail.com>
2026-02-07 00:01:41 +0400

    лабиринт больше не идеальный

--- src/engine/maze_generation.py
+++ src/engine/maze_generation.py
@@ -22,7 +22,7 @@ class MazeChecker:
         else:
             return False
 
-    def is_visitible(maze: list, x: int, y: int) -> bool:
+    def is_visitable(maze: list, x: int, y: int) -> bool:
         """"""
 
         if MazeChecker.is_point_in_maze(maze, x, y) and not MazeChecker.is_road(maze, x, y):
@@ -48,6 +48,8 @@ class MazeGenerator:
             self._connect(x, y)
             self._add_visit_points(x, y)
 
+        self._break_walls(chance=0.08)
+
 
 
     def _connect(self, x, y):
@@ -58,5 +60,47 @@ class MazeGenerator:
 
     def _add_visit_points(self, x, y):
         for dir in self.dirs:
-            if MazeChecker.is_visitible(self.maze, x + dir[0] * 2, y + dir[1] * 2):
+            if MazeChecker.is_visitable(self.maze, x + dir[0] * 2, y + dir[1] * 2):
                 self.to_visit.add((x + dir[0] * 2, y + dir[1] * 2))
+
+    def _break_walls(self, chance=0.1):
+        size = len(self.maze)
+    
+        for y in range(1, size - 1):
+            for x in range(1, size - 1):
+            
+                if self.maze[y][x] != 0:
+                    continue
+                
+                broke = False
+    
+                # горизонтальное соединение
+                if self.maze[y][x - 1] and self.maze[y][x + 1]:
+                    if random.random() < chance:
+                        self.maze[y][x] = 1
+                        if self._creates_room(x, y):
+                            self.maze[y][x] = 0
+                        else:
+                            broke = True
+    
+                # вертикальное соединение
+                if not broke and self.maze[y - 1][x] and self.maze[y + 1][x]:
+                    if random.random() < chance:
+                        self.maze[y][x] = 1
+                        if self._creates_room(x, y):
+                            self.maze[y][x] = 0
+
+    def _creates_room(self, x, y):
+        # проверяем все 2x2 квадраты, в которые входит (x, y)
+        for dy in (-1, 0):
+            for dx in (-1, 0):
+                cx, cy = x + dx, y + dy
+                if 0 <= cx < len(self.maze) - 1 and 0 <= cy < len(self.maze) - 1:
+                    if (
+                        self.maze[cy][cx] and
+                        self.maze[cy][cx + 1] and
+                        self.maze[cy + 1][cx] and
+                        self.maze[cy + 1][cx + 1]
+                    ):
+                        return True
+        return False
